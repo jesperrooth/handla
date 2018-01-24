ef50678cca03cd4e2ab1666c9e99a479beb8ed9e
jesperrooth <35741733+jesperrooth@users.noreply.github.com>
2018-01-24 18:07:56 +0100

    changed mysql connect commands

--- index.php
+++ index.php
@@ -68,8 +68,10 @@ define( "DB_USER",      getenv('OPENSHIFT_MYSQL_DB_USERNAME') );
 define( "DB_PASSWORD",  getenv('OPENSHIFT_MYSQL_DB_PASSWORD') );
 define( "DB_DATABASE",  getenv('OPENSHIFT_APP_NAME') );
 
-mysql_connect(DB_SERVER,DB_USER,DB_PASSWORD) or die(mysql_error());
-mysql_select_db(DB_DATABASE) or die(mysql_error());
+//mysql_connect(DB_SERVER,DB_USER,DB_PASSWORD) or die(mysql_error());
+//mysql_select_db(DB_DATABASE) or die(mysql_error());
+
+mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
 
 $query = mysql_query("select 1 from items");
 if($query == FALSE) {
