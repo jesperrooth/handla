de9ab20e1f462e8673adf6771159a8f5d2e0b163
jesperrooth <35741733+jesperrooth@users.noreply.github.com>
2018-01-28 21:01:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -116,7 +116,7 @@ $result = mysqli_query($connection, "SELECT id, item, category FROM items ORDER 
 	    
 echo "<br>";
 echo "<fieldset>";
-while ($line = mysqli_fetch_array($result->MYSQLI_BOTH))	
+while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC))	
 {
   echo "<div class=\"row\">";
   echo "<label>$line[item]</label>";
